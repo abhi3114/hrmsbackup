0456c067d125ab1d30485876a4c2f74219cdcea0
Swatantra Jha <swatantra.jha@propertypistol.com>
2019-01-11 16:24:45 +0530

    this commit refs changes for sj-dev1

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,7 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { ToastrModule } from 'ng6-toastr-notifications';
+import { Routes, RouterModule } from '@angular/router';
 import { ReactiveFormsModule , FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login',component: LoginComponent }
+];
 
 @NgModule({
   imports: [
@@ -10,10 +17,13 @@ import { ReactiveFormsModule , FormsModule } from '@angular/forms';
     FormsModule,
     ReactiveFormsModule,
     ToastrModule.forRoot(),
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   declarations: [LoginComponent],
   exports: [
-    LoginComponent
+    LoginComponent,
+    RouterModule
   ]
 })
 export class CoreModule { }
