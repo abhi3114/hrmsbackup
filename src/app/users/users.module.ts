34c288de15e25fc98eceed1bb879ac2e6557674d
Swatantra Jha <swatantra.jha@propertypistol.com>
2019-01-12 11:24:19 +0530

    THis commit refs changes for login

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DataTablesModule } from 'angular-datatables';
 import { ModalModule } from 'ngx-bootstrap';
 import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
@@ -8,23 +9,33 @@ import { LeavesComponent } from './leaves/leaves.component';
 import { ApprovedLeavesComponent } from './approved-leaves/approved-leaves.component';
 import { UnapprovedLeavesComponent } from './unapproved-leaves/unapproved-leaves.component';
 import { MasterComponent } from './master/master.component';
+import { OwlDateTimeModule, OwlNativeDateTimeModule,OWL_DATE_TIME_LOCALE } from 'ng-pick-datetime';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { UserRoutingModule } from './user-routing.module';
+import { ReactiveFormsModule , FormsModule } from '@angular/forms';
 
 
 @NgModule({
   imports: [
   CommonModule,
   DataTablesModule,
+  UserRoutingModule,
   ModalModule.forRoot(),
   BsDatepickerModule.forRoot(),
-  CollapseModule.forRoot()
+  CollapseModule.forRoot(),
+  FormsModule,
+  ReactiveFormsModule,
+  BrowserAnimationsModule
   ],
   declarations: [
-  LeavesComponent,
-  ApprovedLeavesComponent,
-  UnapprovedLeavesComponent,
-  DashboardComponent,
-  MasterComponent
-  ]
-  })
+    LeavesComponent,
+    ApprovedLeavesComponent,
+    UnapprovedLeavesComponent,
+    DashboardComponent,
+    MasterComponent,
+    OwlDateTimeModule,
+    OwlNativeDateTimeModule
+  ],
+  providers: [{provide: OWL_DATE_TIME_LOCALE, useValue: 'en-IN'}],
+})
 export class UsersModule { }
