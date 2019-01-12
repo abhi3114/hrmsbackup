af1749bdaa5a78f975cd3e4200150ef6ff68ab63
Swatantra Jha <swatantra.jha@propertypistol.com>
2019-01-12 20:26:59 +0530

    This commit refs changes for sj-dev1

--- src/app/managers/managers.module.ts
+++ src/app/managers/managers.module.ts
@@ -1,15 +1,34 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SubordinateLeavesModule } from './subordinate-leaves/subordinate-leaves.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DataTablesModule } from 'angular-datatables';
+import { ModalModule } from 'ngx-bootstrap';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+import { CollapseModule } from 'ngx-bootstrap';
+import { OwlDateTimeModule, OwlNativeDateTimeModule,OWL_DATE_TIME_LOCALE } from 'ng-pick-datetime';
 import { ManagerRoutingModule } from './manager-routing.module'
+import { ReactiveFormsModule , FormsModule } from '@angular/forms';
+import { ApprovedComponent } from './subordinate-leaves/approved/approved.component';
+import { UnapprovedComponent } from './subordinate-leaves/unapproved/unapproved.component';
 
 
 @NgModule({
   imports: [
     CommonModule,
-    SubordinateLeavesModule,
-    ManagerRoutingModule
+    DataTablesModule,
+    ManagerRoutingModule,
+    ModalModule.forRoot(),
+    BsDatepickerModule.forRoot(),
+    CollapseModule.forRoot(),
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    OwlDateTimeModule,
+    OwlNativeDateTimeModule
   ],
-  declarations: []
+  declarations: [
+    ApprovedComponent,
+    UnapprovedComponent
+  ]
 })
 export class ManagersModule { }
--- src/app/managers/subordinate-leaves/approved/approved.component.ts
+++ src/app/managers/subordinate-leaves/approved/approved.component.ts
@@ -1,4 +1,10 @@
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import {Observable,Subject} from 'rxjs';
+import { ApprovedService } from './approved.service';
+import { MonthYearService } from '../../../shared/service/month-year.service';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-approved',
@@ -7,9 +13,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ApprovedComponent implements OnInit {
 
-  constructor() { }
+  isCollapsed = false;
+  approvedLeavesForm: FormGroup;
+  approvedLeavesData={start_date:'',end_date:''};
+  approved_leaves_data:any;approvedleavesData:any; showDataTable:Boolean;
+  leaveTableOptions: DataTables.Settings = {};
+  leaveTableTrigger: Subject<any> = new Subject();
+
+  constructor(private router:Router,private api:ApprovedService,private monthandyear:MonthYearService) {
+    var filteredData=monthandyear.getFilterData();
+    this.approvedLeavesData.start_date = filteredData[0].firstDay;
+    this.approvedLeavesData.end_date = filteredData[0].lastDay;
+    this.approvedLeavesForm = new FormGroup({
+      start_date: new FormControl('', [Validators.required]),
+      end_date: new FormControl('', [Validators.required]),
+    });
+  }
+
+  validateApprovedLeavesForm(){
+    var start_date=moment(this.approvedLeavesData.start_date).format('DD/MM/YYYY');
+    var end_date=moment(this.approvedLeavesData.end_date).format('DD/MM/YYYY');
+    this.api.getallapprovedSubordinateLeave(start_date,end_date).subscribe(res => {
+      this.approved_leaves_data=res;
+      this.approvedLeavesData=this.approved_leaves_data.approved_leaves_data;
+      }, (err) => {
+        alert(err.error);
+    });
+  }
 
   ngOnInit() {
+    var start_date=moment(this.approvedLeavesData.start_date).format('DD/MM/YYYY');
+    var end_date=moment(this.approvedLeavesData.end_date).format('DD/MM/YYYY');
+    this.api.getallapprovedSubordinateLeave(start_date,end_date).subscribe(res => {
+      this.approved_leaves_data=res;
+      this.approvedLeavesData=this.approved_leaves_data;
+      this.leaveTableOptions = {
+        pagingType: 'full_numbers',
+        pageLength: 10
+      };
+      this.leaveTableTrigger.next();
+      }, (err) => {
+      alert(err.error);
+    });
   }
 
 }
