f2637a42e0d85da05d1803b4418a9ba80071174c
Swatantra Jha <swatantra.jha@propertypistol.com>
2019-01-12 16:58:37 +0530

    This commit refs fixes

--- src/app/managers/manager-routing.module.ts
+++ src/app/managers/manager-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ApprovedComponent } from './subordinate-leaves/approved/approved.component';
-import { UnapprovedComponent } from './subordinate-leaves/approved/unapproved.component';
+import { UnapprovedComponent } from './subordinate-leaves/unapproved/unapproved.component';
 import { MasterComponent } from '../shared/components/master/master.component';
 
 const routes: Routes = [
