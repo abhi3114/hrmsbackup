ff3ff89cd5f49b2236405b80cfd965fb18f49485
Swatantra Jha <swatantra.jha@propertypistol.com>
2019-01-14 17:45:39 +0530

    This commit refs changes for prod

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MasterComponent } from './components/master/master.component';
 import { NullDataPipe } from './pipes/null-data.pipe';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [MasterComponent, NullDataPipe],
+  declarations: [NullDataPipe],
     exports: [
     NullDataPipe
   ]
