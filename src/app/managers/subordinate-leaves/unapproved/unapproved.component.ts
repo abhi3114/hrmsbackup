9cd86f77c31598956572b89d2696c013d5f42b82
Swatantra Jha <swatantra.jha@propertypistol.com>
2019-01-15 13:55:27 +0530

    This commit refs changes for managers approval

--- src/app/managers/subordinate-leaves/unapproved/unapproved.component.ts
+++ src/app/managers/subordinate-leaves/unapproved/unapproved.component.ts
@@ -4,6 +4,7 @@ import { Component, OnInit } from '@angular/core';
 import {Observable, Subject } from 'rxjs'
 import {UnapprovedService} from './unapproved.service'
 import { MonthYearService } from '../../../shared/service/month-year.service';
+import { NotificationService } from '../../../shared/service/notification.service';
 import * as moment from 'moment';
 
 
@@ -16,30 +17,15 @@ export class UnapprovedComponent implements OnInit {
 
   isCollapsed = false;
   leaveids = [];
-  unapprovedLeavesForm: FormGroup;
   unapprovedLeavesData={start_date:'',end_date:''};
   unapproved_leaves_data:any;unapprovedleavesData:any; showDataTable:Boolean;
   leaveTableOptions: DataTables.Settings = {};
   leaveTableTrigger: Subject<any> = new Subject();
 
-  constructor(private router:Router, private api:UnapprovedService, private my:MonthYearService) {
+  constructor(private router:Router, private api:UnapprovedService, private my:MonthYearService, private notification:NotificationService) {
     var filteredData=my.getFilterData();
     this.unapprovedLeavesData.start_date = filteredData[0].firstDay;
     this.unapprovedLeavesData.end_date = filteredData[0].lastDay;
-    this.unapprovedLeavesForm = new FormGroup({
-      start_date: new FormControl('', [Validators.required]),
-      end_date: new FormControl('', [Validators.required]),
-    });
-  }
-
-  validateApprovedLeavesForm(){
-    var start_date=moment(this.unapprovedLeavesData.start_date).format('DD/MM/YYYY');
-    var end_date=moment(this.unapprovedLeavesData.end_date).format('DD/MM/YYYY');
-    this.api.getAllUnapprovedSubordinateLeave(start_date,end_date).subscribe(res => {
-      this.unapproved_leaves_data=res;
-      }, (err) => {
-        alert(err.error);
-    });
   }
 
   ngOnInit() {
@@ -65,8 +51,32 @@ export class UnapprovedComponent implements OnInit {
   }
 
   save(){
+    var leave_ids = []
     $('.checkbox:checked').each(function() {
+      var id = $(this).attr('name');
+      leave_ids.push(id);
     });
+    var postdata = { "leave_ids":  leave_ids}
+    if(leave_ids != undefined && leave_ids.length > 0)
+    {
+      this.api.sendForLeaveApproval(postdata).subscribe(res => {
+        leave_ids = []
+        var start_date=moment(this.unapprovedLeavesData.start_date).format('DD/MM/YYYY');
+        var end_date=moment(this.unapprovedLeavesData.end_date).format('DD/MM/YYYY');
+        this.api.getAllUnapprovedSubordinateLeave(start_date,end_date).subscribe(res => {
+          this.unapproved_leaves_data=res;
+          }, (err) => {
+          alert(err.error);
+        });
+        this.notification.showSuccess('Leave approved successfully');
+        }, (err) => {
+        this.notification.showError('err.error');
+      });
+    }
+    else
+    {
+      this.notification.CustomErrorMessage('Please check atleast one unapproved leave');
+    }
   }
 
 }
