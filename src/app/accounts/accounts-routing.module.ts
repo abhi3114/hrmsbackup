45f546d1528b715029eca09207d70351cbce9b66
abhiping <pingleabhishek3@gmail.com>
2019-01-16 19:04:01 +0530

    Salary Report Changes

--- src/app/accounts/accounts-routing.module.ts
+++ src/app/accounts/accounts-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { SalaryProcessingComponent } from './salary-processing/salary-processing.component'
 import { MasterComponent } from '../shared/components/master/master.component';
 import { EditSalaryComponent } from './edit-salary/edit-salary.component';
+import { SalaryReportComponent } from './salary-report/salary-report.component';
 const routes: Routes = [
 {
   path: 'home', component: MasterComponent,
   children: [
   {path: 'users/salaries', component: SalaryProcessingComponent},
-  {path: 'users/:id', component: EditSalaryComponent}
+  {path: 'users/:id', component: EditSalaryComponent},
+  {path: 'users/report/salary_report', component: SalaryReportComponent}
   ]
 }
 ];
--- src/app/accounts/accounts.module.ts
+++ src/app/accounts/accounts.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
 import { DataTablesModule } from 'angular-datatables';
 import { ModalModule } from 'ngx-bootstrap';
 import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
@@ -9,8 +10,8 @@ import { OwlDateTimeModule, OwlNativeDateTimeModule } from 'ng-pick-datetime';
 import { ReactiveFormsModule , FormsModule } from '@angular/forms';
 import { AccountsRoutingModule } from './accounts-routing.module';
 import { SalaryProcessingComponent } from './salary-processing/salary-processing.component'
-import {MatSlideToggleModule} from '@angular/material/slide-toggle';
 import { EditSalaryComponent } from './edit-salary/edit-salary.component';
+import { SalaryReportComponent } from './salary-report/salary-report.component';
 @NgModule({
     imports: [
     CommonModule,
@@ -26,6 +27,6 @@ import { EditSalaryComponent } from './edit-salary/edit-salary.component';
     OwlNativeDateTimeModule,
     MatSlideToggleModule
     ],
-    declarations: [SalaryProcessingComponent, EditSalaryComponent]
+    declarations: [SalaryProcessingComponent, EditSalaryComponent, SalaryReportComponent]
     })
 export class AccountsModule { }
