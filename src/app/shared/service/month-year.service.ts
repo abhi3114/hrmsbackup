bd0762e6b84e0d9ab52f281de1c1fd6f8df4a52a
Swatantra Jha <swatantra.jha@propertypistol.com>
2019-01-16 13:01:55 +0530

    This commit refs changes for salary - date

--- src/app/shared/service/month-year.service.ts
+++ src/app/shared/service/month-year.service.ts
@@ -32,28 +32,6 @@ export class MonthYearService {
     var filterData=[];filterData.push({firstDay:firstDay,lastDay:lastDay});
     return filterData;
   }
-  getMonthandYear()
-  {
-    var currentDate=moment();
-    var end = new Date().getFullYear();
-    var startOfMonth = moment().startOf('month');
-    var tenthDayOfMonth=moment(startOfMonth).add(10,'d');
-    if (currentDate.isBetween(startOfMonth, tenthDayOfMonth))
-    {
-      var currentmonth= new Date().getMonth();
-      if(currentmonth == 0){var currentmonth= 12; var y=(end-1).toString();}else{var y=end.toString();}
-    }
-    else
-    {
-      var currentmonth= new Date().getMonth()+1; var y=end.toString();
-    }
-
-    var selectedmonth= currentmonth.toString();
-    var selectedyear=y;
-
-    var data={selectedmonth,selectedyear}
-    return data
-  }
 
   getSalaryEditableRange(selectedmonth,selectedyear)
   {
