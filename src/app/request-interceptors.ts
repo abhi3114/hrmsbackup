b6467e55e2f4543448bebfa4607765fb8c0a31e2
abhiping <pingleabhishek3@gmail.com>
2019-01-22 13:05:54 +0530

    Http Interceptor for invalid authentication

--- src/app/request-interceptors.ts
+++ src/app/request-interceptors.ts
@@ -7,37 +7,38 @@ import {
   HttpInterceptor,
   HttpResponse,
   HttpErrorResponse
-} from "@angular/common/http";
-import { Observable } from "rxjs/Observable";
+  } from "@angular/common/http";
+  import { Observable } from "rxjs/Observable";
+  import { Router } from '@angular/router';
 
-@Injectable()
-export class MyInterceptor implements HttpInterceptor {
-  constructor() { }
-  //function which will be called for all http calls
-  intercept(
-    request: HttpRequest<any>,
-    next: HttpHandler
-  ): Observable<HttpEvent<any>> {
-    //how to update the request Parameters
-    const updatedRequest = request.clone({
-    });
-    //logging the updated Parameters to browser's console
-    console.log("Before making api call : ", updatedRequest);
-    return next.handle(request).pipe(
-      tap(
-        event => {
-          //logging the http response to browser's console in case of a success
-          if (event instanceof HttpResponse) {
+  @Injectable()
+  export class MyInterceptor implements HttpInterceptor {
+    constructor(private router:Router) { }
+    //function which will be called for all http calls
+    intercept(
+      request: HttpRequest<any>,
+      next: HttpHandler
+      ): Observable<HttpEvent<any>> {
+      //how to update the request Parameters
+      const updatedRequest = request.clone({
+        });
+      return next.handle(request).pipe(
+        tap(
+          event => {
+            //logging the http response to browser's console in case of a success
+            if (event instanceof HttpResponse) {
+            }
+            },
+            error => {
+              //logging the http response to browser's console in case of a failuer
+              console.log(error.status);
+              if(error.status === 401)
+              {
+                this.router.navigateByUrl('/login');
+              }
 
-          }
-        },
-        error => {
-          //logging the http response to browser's console in case of a failuer
-          if (event instanceof HttpResponse) {
-
-          }
-        }
-      )
-    );
+            }
+            )
+        );
+    }
   }
-}
