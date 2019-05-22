f43b7fac6848b749c0ac4bcf87fd460debb899f8
Harshal Ladhe <harshal.ladhe.91@live.co.uk>
2019-05-22 18:37:08 +0530

    This commit refs to display approved outdoor duties on modal

--- src/app/managers/subordinate-outdoor-duties/approved-outdoor-duties/approved-outdoor-duties.component.ts
+++ src/app/managers/subordinate-outdoor-duties/approved-outdoor-duties/approved-outdoor-duties.component.ts
@@ -1,10 +1,12 @@
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { Component, OnInit,ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, TemplateRef} from '@angular/core';
 import {Observable,Subject} from 'rxjs';
 import { DataTableDirective } from 'angular-datatables';
 import { NotificationService } from '../../../shared/service/notification.service';
 import { ApprovedOutdoorDutyService } from './approved-outdoor-duty.service';
+import { BsModalService } from 'ngx-bootstrap/modal';
+import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
 import { MonthYearService } from '../../../shared/service/month-year.service';
 import * as moment from 'moment';
 
@@ -18,13 +20,15 @@ export class ApprovedOutdoorDutiesComponent implements OnInit {
   isCollapsed = false;
   approvedOutDoorDutyForm: FormGroup;
   approvedOutDoorDutyData={start_date:'',end_date:''};
-  approved_outdoor_duty_data:any;approvedoutdoordutydata:any; showDataTable:Boolean;
+  approved_outdoor_duty_data:any; showDataTable:Boolean;
+  user_approved_outdoor_data:any;
   @ViewChild(DataTableDirective)
   dtElement: DataTableDirective;
   leaveTableOptions: DataTables.Settings = {};
   leaveTableTrigger: Subject<any> = new Subject();
+  modalRef: BsModalRef;
 
-  constructor(private router:Router,private api:ApprovedOutdoorDutyService,private monthandyear:MonthYearService, private notification:NotificationService) {
+  constructor(private router:Router,private api:ApprovedOutdoorDutyService,private monthandyear:MonthYearService, private notification:NotificationService, private modalService: BsModalService) {
     var filteredData=monthandyear.getFilterData();
     this.approvedOutDoorDutyData.start_date = filteredData[0].firstDay;
     this.approvedOutDoorDutyData.end_date = filteredData[0].lastDay;
@@ -37,6 +41,19 @@ export class ApprovedOutdoorDutiesComponent implements OnInit {
       lengthMenu: [[5, 10, 20, 50,-1],
       [5, 10, 20, 50,"All" ]]
     };
+  }
+
+  userOutdoorsList(template: TemplateRef<any>, l)
+  {
+    this.modalRef = this.modalService.show(template);
+    var start_date=moment(this.approvedOutDoorDutyData.start_date).format('DD/MM/YYYY');
+    var end_date=moment(this.approvedOutDoorDutyData.end_date).format('DD/MM/YYYY');
+    var user_id = l.user_id
+    this.api.getAllApprovedSpecificSubordinateOutdoors(start_date,end_date, user_id).subscribe(res => {
+      this.user_approved_outdoor_data=res;
+    }, (err) => {
+      this.notification.showError(err.error);
+    });
   }
 
   validateApprovedOutDoorDutyForm(){
@@ -54,7 +71,7 @@ export class ApprovedOutdoorDutiesComponent implements OnInit {
     var start_date=moment(this.approvedOutDoorDutyData.start_date).format('DD/MM/YYYY');
     var end_date=moment(this.approvedOutDoorDutyData.end_date).format('DD/MM/YYYY');
     this.api.getallapprovedOutDoorDuties(start_date,end_date).subscribe(res => {
-      this.approved_outdoor_duty_data=res;      
+      this.approved_outdoor_duty_data=res;
       this.leaveTableTrigger.next();
     }, (err) => {
       this.notification.showError(err.error);
--- src/app/managers/subordinate-outdoor-duties/approved-outdoor-duties/approved-outdoor-duty.service.ts
+++ src/app/managers/subordinate-outdoor-duties/approved-outdoor-duties/approved-outdoor-duty.service.ts
@@ -14,5 +14,10 @@ export class ApprovedOutdoorDutyService {
       { headers: new HttpHeaders({"Authorization": 'Token token=' + localStorage.getItem('token'),
         "Content-Type": 'application/json'})})
   }
-
+  getAllApprovedSpecificSubordinateOutdoors(start_date,end_date, user_id)
+  {
+    return this.http.get(environment.baseUrl+ "managers/outdoor_duties/users/"+user_id+"/approved?start_date="+start_date+"&end_date="+end_date,
+      { headers: new HttpHeaders({"Authorization": 'Token token=' + localStorage.getItem('token'),
+        "Content-Type": 'application/json'})})
+  }
 }
