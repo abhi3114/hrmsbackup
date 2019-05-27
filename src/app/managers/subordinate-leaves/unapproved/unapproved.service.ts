826e9a3d76d40fb512d1f65a003d32d2e5b00c5f
Harshal Ladhe <harshal.ladhe.91@live.co.uk>
2019-05-27 16:01:14 +0530

    This commit refs to implementing approve/reject functionality for leaves

--- src/app/managers/subordinate-leaves/unapproved/unapproved.service.ts
+++ src/app/managers/subordinate-leaves/unapproved/unapproved.service.ts
@@ -10,14 +10,44 @@ export class UnapprovedService {
   constructor(private http:HttpClient) {
   }
 
-  getAllUnapprovedSubordinateLeave(start_date, end_date){
-    return this.http.get(environment.baseUrl+ "leaves/manager/unapproved?start_date="+start_date+"&end_date="+end_date,
+  getAllUnapprovedSubordinateLeaves(start_date, end_date){
+    return this.http.get(environment.baseUrl+ "managers/leaves/unapproved?start_date="+start_date+"&end_date="+end_date,
       { headers: new HttpHeaders({"Authorization": 'Token token=' + localStorage.getItem('token'),
         "Content-Type": 'application/json'})})
   }
 
-  sendForLeaveApproval(params){
-    return this.http.post(environment.baseUrl+ "leaves/manager/unapproved",params, {
+  getAllUnApprovedSpecificSubordinateLeaves(start_date,end_date, user_id) {
+    return this.http.get(environment.baseUrl+ "managers/leaves/users/"+user_id+"/unapproved?start_date="+start_date+"&end_date="+end_date,
+      { headers: new HttpHeaders({"Authorization": 'Token token=' + localStorage.getItem('token'),
+        "Content-Type": 'application/json'})})
+  }
+
+  sendForSingleLeaveApproval(leave_id) {
+    return this.http.post(environment.baseUrl+ "managers/leaves/"+leave_id+"/approve",leave_id,{
+      headers: {
+        "Authorization": 'Token token=' + localStorage.getItem('token'),
+        "Content-Type": 'application/json'
+      }
+    })
+  }
+
+  sendForSingleLeaveRejection(leave_id) {
+    return this.http.delete(environment.baseUrl+ "managers/leaves/"+leave_id+"/reject",
+      { headers: new HttpHeaders({"Authorization": 'Token token=' + localStorage.getItem('token'),
+        "Content-Type": 'application/json'})})
+  }
+
+  sendForBulkLeavesApproval(params){
+    return this.http.post(environment.baseUrl+ "managers/leaves/bulk_approve",params,{
+      headers: {
+        "Authorization": 'Token token=' + localStorage.getItem('token'),
+        "Content-Type": 'application/json'
+      }
+    })
+  }
+
+  sendForBulkLeavesRejection(params){
+    return this.http.post(environment.baseUrl+ "managers/leaves/bulk_reject",params,{
       headers: {
         "Authorization": 'Token token=' + localStorage.getItem('token'),
         "Content-Type": 'application/json'
