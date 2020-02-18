69ddd5951d4aef8048d8136195084087e73df18b
AnkushT27 <ankushtiwari102@gmail.com>
2020-02-18 20:21:21 +0530

    rembursement-fixes

--- src/app/users/reimbursement/custom-field-select/custom-field-select.component.ts
+++ src/app/users/reimbursement/custom-field-select/custom-field-select.component.ts
@@ -5,27 +5,20 @@ import { FieldType } from '@ngx-formly/core';
 @Component({
   selector: 'app-custom-field-select',
   template: `
-    <div [formGroup]="clientNameField">
-      <label>Expense For<sup style="color:red;">*</sup></label>
+    <label>Expense For<sup style="color:red;">*</sup></label>
         <select (change)="toggleDeppendentField($event)"
          class="select2 form-control" style="width: 100%; height: 35px;" [formControl]="formControl" [formlyAttributes]="field">
-         <option value="" disabled="disabled">Select item</option>
+         <option value="" disabled="disabled">Select</option>
          <option *ngFor="let e of expanseFor" value="{{e.id}}">{{e.value}}</option>
         </select>
       <label *ngIf="canShowField">Client Name<sup style="color:red;">*</sup></label>
-     <input *ngIf="canShowField"  type="text" class="form-control"  formControlName="client_name">
-     
- `,
+     <input *ngIf="canShowField" type="text" class="client_name form-control" formControlName="client_name">
+   `,
 })
 export class CustomFieldSelectComponent extends FieldType {
   canShowField:boolean = false;
   expanseFor:any = [{"id": "client", "value": "Client"}, {"id": "self", "value": "Self"}]
-
-  clientNameField = new FormGroup({
-    client_name: new FormControl(''),
-  });
-
-  toggleDeppendentField($event){
+ toggleDeppendentField($event){
     if ($event.target.value == 'client'){
       this.canShowField = true;
     }
