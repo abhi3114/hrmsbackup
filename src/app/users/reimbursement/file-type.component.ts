c55048f93f8fed0fdcbf8fa24a02d8886e89eeb7
AnkushT27 <ankushtiwari102@gmail.com>
2020-02-18 12:33:58 +0530

    form reset and file type required changes

--- src/app/users/reimbursement/file-type.component.ts
+++ src/app/users/reimbursement/file-type.component.ts
@@ -4,7 +4,8 @@ import { FieldType } from '@ngx-formly/core';
 @Component({
   selector: 'formly-field-file',
   template: `
-    <input type="file" [formlyAttributes]="field">
+   <input type="file" [formlyAttributes]="field">
+   <span style='color:red;'>*</span>
   `
 })
 export class FormlyFieldFile extends FieldType {}
